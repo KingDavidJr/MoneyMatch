e515f20eac629696a0d56c302fff7309ed7d355a
David <realkd872@gmail.com>
2022-09-12 10:04:46 -0500

    Initial Commit

---
+++ MoneyMatch/MoneyMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMatchApp.swift
+//  MoneyMatch
+//
+//  Created by David Amedeka on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
