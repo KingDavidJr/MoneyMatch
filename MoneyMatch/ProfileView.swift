4d4b88768c8086a76fd4ad17181984cb8ee6109f
David <realkd872@gmail.com>
2022-09-15 12:07:46 -0500

    buttons, profile circle view

--- MoneyMatch/ProfileView.swift
+++ MoneyMatch/ProfileView.swift
@@ -9,15 +9,36 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        ZStack {
+        let name = "Daniel"
+        ZStack() {
             Color("DefaultBackgroundColor")
                 .ignoresSafeArea()
-            VStack {
+            VStack(spacing: 50) {
                 Text("Profile")
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .foregroundColor(.white)
+                
+                
+                Text("Hi \(name)!")
+                    .font(.title)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                
+                
+                ProfileCircleView()
+                
+                
+                Text("Options!")
+                    .font(.title)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .underline()
+                
+                ButtonView()
+                //Spacer()
             }
+            
         }
     }
 }
@@ -25,5 +46,75 @@ struct ProfileView: View {
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+struct ProfileCircleView: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.white)
+            Circle()
+                .stroke(.black, lineWidth: 7)
+        }
+        .frame(minWidth: 200,
+               maxWidth: 250,
+               minHeight: 200,
+               maxHeight: 250)
+    }
+}
+
+struct ButtonView: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Button(action: {
+                
+            }, label: {
+                Text("Character")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color("ButtonColors"))
+                            .frame(minWidth: 200,
+                                   maxWidth: 250,
+                                   minHeight: 50,
+                                   maxHeight: 60)
+                    )
+            })
+            Button(action: {
+                
+            }, label: {
+                Text("Colors")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color("ButtonColors"))
+                            .frame(minWidth: 200,
+                                   maxWidth: 250,
+                                   minHeight: 50,
+                                   maxHeight: 60)
+                    )
+            })
+            Button(action: {
+                
+            }, label: {
+                Text("Settings")
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .fontWeight(.light)
+                    .background(
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color("ButtonColors"))
+                            .frame(minWidth: 200,
+                                   maxWidth: 250,
+                                   minHeight: 50,
+                                   maxHeight: 60)
+                    )
+            })
+        }
     }
 }
