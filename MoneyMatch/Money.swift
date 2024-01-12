53fd8e817782470857bd6c94749a6dad53a584bd
David <realkd872@gmail.com>
2024-01-12 12:28:24 -0600

    Money Model

--- MoneyMatch/Money.swift
+++ MoneyMatch/Money.swift
@@ -7,4 +7,42 @@
 
 import Foundation
 
+enum Money {
+    case penny
+    case nickel
+    case dime
+    case quarter
+    case oneDollarBill
+    case fiveDollarBill
+    case tenDollarBill
+    case twentyDollarBill
+    case fiftyDollarBill
+    case hundredDollarBill
+}
 
+extension Money {
+    func currencyValue() -> Float {
+        switch self {
+        case .penny:
+            return 0.01
+        case .nickel:
+            return 0.05
+        case .dime:
+            return 0.10
+        case .quarter:
+            return 0.25
+        case .oneDollarBill:
+            return 1.00
+        case .fiveDollarBill:
+            return 5.00
+        case .tenDollarBill:
+            return 10.00
+        case .twentyDollarBill:
+            return 20.00
+        case .fiftyDollarBill:
+            return 50.00
+        case .hundredDollarBill:
+            return 100.00
+        }
+    }
+}
