7eac107935074fa1524618d14d56e72855fd3862
David <realkd872@gmail.com>
2024-01-12 12:58:06 -0600

    Moved function to view model

--- MoneyMatch/Money.swift
+++ MoneyMatch/Money.swift
@@ -19,30 +19,3 @@ enum Money {
     case fiftyDollarBill
     case hundredDollarBill
 }
-
-extension Money {
-    func currencyValue() -> Float {
-        switch self {
-        case .penny:
-            return 0.01
-        case .nickel:
-            return 0.05
-        case .dime:
-            return 0.10
-        case .quarter:
-            return 0.25
-        case .oneDollarBill:
-            return 1.00
-        case .fiveDollarBill:
-            return 5.00
-        case .tenDollarBill:
-            return 10.00
-        case .twentyDollarBill:
-            return 20.00
-        case .fiftyDollarBill:
-            return 50.00
-        case .hundredDollarBill:
-            return 100.00
-        }
-    }
-}
