39aedc85e32a1b5cd495d786d44b66fe0f9eae91
David <realkd872@gmail.com>
2024-01-12 13:45:45 -0600

    Added level examples -> Changed String to Float

--- MoneyMatch/Level.swift
+++ MoneyMatch/Level.swift
@@ -10,11 +10,19 @@ import Foundation
 struct Level: Identifiable {
     let id: Int
     let itemName: String
-    let itemPrice: String
+    let itemPrice: Float
     
-    init(id: Int, itemName: String, itemPrice: String) {
+    init(id: Int, itemName: String, itemPrice: Float) {
         self.id = id
         self.itemName = itemName
         self.itemPrice = itemPrice
     }
+}
+
+struct LevelHelper {
+    static let levelsExample: [Level] = [
+        Level(id: 1, itemName: "toy", itemPrice: 22.0),
+        Level(id: 2, itemName: "candy", itemPrice: 2.0),
+    
+    ]
 }
