b6e2450c9245f37fecf9ad72d52b5025c0b4014a
David <realkd872@gmail.com>
2024-01-12 13:44:46 -0600

    Change to how data is managed

--- MoneyMatch/SituationViewModel.swift
+++ MoneyMatch/SituationViewModel.swift
@@ -7,34 +7,45 @@
 
 import Foundation
 
-@MainActor class SituationViewModel {
+@MainActor class SituationViewModel: ObservableObject {
+    static var shared = SituationViewModel()
     
-    func addValue(of money: Money) -> Float {
+    @Published var currentValue: Float
+    
+    init(currentValue: Float = 0.0) {
+        self.currentValue = currentValue
+    }
+    
+    func addValue(of money: Money) {
         switch money {
         case .penny:
-            return 0.01
+            currentValue += 0.01
         case .nickel:
-            return 0.05
+            currentValue += 0.05
         case .dime:
-            return 0.10
+            currentValue += 0.10
         case .quarter:
-            return 0.25
+            currentValue += 0.25
         case .oneDollarBill:
-            return 1.00
+            currentValue += 1.00
         case .fiveDollarBill:
-            return 5.00
+            currentValue += 5.00
         case .tenDollarBill:
-            return 10.00
+            currentValue += 10.00
         case .twentyDollarBill:
-            return 20.00
+            currentValue += 20.00
         case .fiftyDollarBill:
-            return 50.00
+            currentValue += 50.00
         case .hundredDollarBill:
-            return 100.00
+            currentValue += 100.00
         }
     }
     
-    func checkValue(value: Float) {
+    func checkValue(level: Level.ID) {
         
+    }
+    
+    func resetValue() {
+        currentValue = 0.0
     }
 }
