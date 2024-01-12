a21b8667236eb652d95ad988eb6b93826fa067ec
David <realkd872@gmail.com>
2024-01-12 13:44:19 -0600

    init

---
+++ MoneyMatch/LevelCardView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelCardView.swift
+//  MoneyMatch
+//
+//  Created by David Amedeka on 1/12/24.
+//
+
+import SwiftUI
+
+struct LevelCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelCardView()
+}
