ee032364ce71505e9a96c5f3c72a8f0bb8f5cef8
David <realkd872@gmail.com>
2024-01-12 12:31:44 -0600

    init

---
+++ MoneyMatch/SituationViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SituationViewModel.swift
+//  MoneyMatch
+//
+//  Created by David Amedeka on 1/12/24.
+//
+
+import Foundation
+
+@MainActor class SituationViewModel {
+    
+    func checkValue(value: Float) {
+        
+    }
+}
