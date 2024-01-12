817501d9112ae1ec6098084d3ff6a68574e17266
David <realkd872@gmail.com>
2024-01-12 12:57:33 -0600

    init files

---
+++ MoneyMatch/ButtonsView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonsView.swift
+//  MoneyMatch
+//
+//  Created by David Amedeka on 1/12/24.
+//
+
+import SwiftUI
+
+struct ButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonsView()
+}
