08655e8a89d26c464a03d5df0e3762b6cf7d5116
David <realkd872@gmail.com>
2024-01-12 13:59:08 -0600

    Added Design for the view

--- MoneyMatch/LevelCardView.swift
+++ MoneyMatch/LevelCardView.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct LevelCardView: View {
+    let level: Level
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Level \(level.id)")
+            .foregroundStyle(.white)
+            .font(.title)
+            .padding()
+            .containerRelativeFrame(.horizontal, { size, axis in
+                size * 0.6
+            })
+            .background {
+                Color.blue
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 5.0))
     }
 }
 
 #Preview {
-    LevelCardView()
+    LevelCardView(level: LevelHelper.levelsExample[0])
 }
